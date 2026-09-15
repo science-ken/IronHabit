d73474634ee224b6306ef34898048de72880fdf9
science-ken <science-ken@users.noreply.github.com>
2026-09-15 11:47:39 +0800

    chore: bump versionCode 4 / versionName 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.ironhabit.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
